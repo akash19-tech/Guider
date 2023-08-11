dc93a29ec754de69631a42eac91c6ae9ad730669
Akash Mishra <aakuakash091@gmail.com>
2023-08-12 01:21:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var a=document.getElementById("loginbtn");
+    var b=document.getElementById("signupbtn");
+    var x=document.getElementById("login");
+    var y=document.getElementById("register");
+
+    function login(){
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        a.className += " white-btn";
+        b.className = "btn";
+        x.style.opacity = 1;
+        y.style.opacity = 0;
+    }
+    function register(){
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        a.className = "btn";
+        b.className += " white-btn";
+        x.style.opacity = 0;
+        y.style.opacity = 1;
+    }
